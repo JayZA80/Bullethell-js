331b2320deb71beaf907798846b4db12f7b37816
Jeremy Penaredondo <jay.pena1224@proton.me>
2024-07-25 18:22:25 -0400

    writes tests for cannon class

---
+++ src/tests/cannonTester.test.js
@@ -0,0 +1,32 @@
+import Cannon from "../main/Models/Player-Generics/Cannon";
+
+const cannon = new Cannon();
+
+test('Expect 100 for cannon\'s base damage', () => {
+    expect(cannon.getBaseDMG()).toBe(100);
+});
+
+test('Expecting secondary cannon to be "true"', () => {
+    cannon.toggleSecondaryCannons();
+    expect(cannon.getSecondaryCannonStatus()).toBe(true);
+});
+
+test('Expecting full armament to be "true"', () => {
+    cannon.toggleFullArmament();
+    expect(cannon.getFullArmamanetStatus()).toBe(true);
+});
+
+test('Expecting to be on stage 3 cannons', () => {
+    cannon.nextStage();
+    cannon.nextStage();
+    cannon.nextStage();
+    cannon.nextStage();
+    cannon.nextStage();
+    cannon.nextStage();
+    expect(cannon.getCurrentStage()).toBe(3);
+});
+
+test('Expect the cannon\'s stage to be reset to 1', () => {
+    cannon.resetStage();
+    expect(cannon.getCurrentStage()).toBe(1);
+});
